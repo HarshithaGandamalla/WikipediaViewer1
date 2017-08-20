286538ed97b63abba6a06af355e5675b0555f521
Harshitha <harshithag15@gmail.com>
2017-08-19 23:38:56 -0400

    First commit:

--- js/main.js
+++ js/main.js
@@ -1,52 +1,63 @@
-$('#form1').focus(function(){
-    $("#form1").addClass("onFocus");
+var f=document.getElementById('form1');
+
+f.addEventListener('focus',function(){
+   f.classList.add("onFocus"); 
 });
 
-$('#form1').blur(function(){
-    $("#form1").addClass("offFocus");
+f.addEventListener('blur',function(){
+    f.classList.remove('onFocus');
+    f.classList.add('offFocus'); 
+
 });
 
-$('h1#random').on('click',function(){
+document.getElementById('random').addEventListener('click',function(){
     window.open("https://en.wikipedia.org/wiki/Special:Random");   
 });
 
-$('#form1').keyup(function(e){
+f.addEventListener('keyup',function(e){
      
+     if(!e) var e=window.event; //for IE
+    
 
      if(e.which == 13)
         {
-
             e.preventDefault();
-            $.ajax({
-                url: 'http://en.wikipedia.org/w/api.php?exintro&explaintext',
-                data: { action: 'query', generator:'search', gsrsearch: $("input#form1").val(), format: 'json',gsrnamespace:0,gsrlimit:10,prop:'pageimages|extracts',pilimit:'max',exsentences:1,exlimit:'max'
-                },
-                dataType: 'jsonp',
-                success: processResult
-            });
+            var script=document.createElement("script");
+            script.src="https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="+f.value+"&callback=JSON_CALLBACK";
+            document.head.appendChild(script);
         }
+    
 });
 
 
 
 
-  function processResult(apiResult){
-                
-                       $("div#display-result").empty();
-      
-                       var results=apiResult.query.pages;
-                
-                       console.log(results);
-                      $.each(results,function(i,val){
-                       var url='https://en.wikipedia.org/?curid='+val.pageid;
-                       var newdiv="<div id="+i+"><strong>"+val.title+"</strong><br>"+val.extract+"</div>";
-                       
-                         $("div#display-result").append("<a href="+url+">"+newdiv+"</a><br>");      
+  function JSON_CALLBACK(apiResult){
+         
+    var display=document.getElementById("display-result");
 
-                     
-                          
-                         
-                   });
+      while(display.firstChild){
+      display.removeChild(display.firstChild);
+    }
+      
+     var results=apiResult.query.pages;
+     for (var key in results) {
+     // skip loop if the property is from prototype
+     //if (!results.hasOwnProperty(key)) continue;
+          
+       var obj = results[key];
+       var url='https://en.wikipedia.org/?curid='+obj.pageid;
+
+       var anchor=document.createElement('a');
+       anchor.href=url;
+         
+       var newdiv="<div><strong>"+obj.title+"</strong><br>"+obj.extract+"</div>";
+       anchor.innerHTML=newdiv;
+
+       display.appendChild(anchor); 
+    
+    }
+                   
       
      
   }
